c6e35b29c898e1324948e741bc522ce7840ca6e9
Ajmal Shah Kashif <ajmalshahkashif@gmail.com>
2020-11-18 01:23:32 +0500

    Project basic folders & files added as base

---
+++ Hospital Management System/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hospital_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string password)
+        {
+            return View();
+        }
+
+    }
+}
