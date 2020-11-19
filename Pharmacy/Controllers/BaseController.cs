7da9b65e9ccb3b896370672bdd8e0941e3a1bef8
Ajmal Shah Kashif <ajmalshahkashif@gmail.com>
2020-11-19 20:57:05 +0500

    Copied data from Pharmacy then deleted unnecessary data

---
+++ Pharmacy/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    public class BaseController : Controller
+    {
+        
+        public BaseController()
+        {
+
+
+        }
+
+    }
+}
---
+++ Pharmacy/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    public class HomeController : BaseController
+    {
+       
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+    }
+}
