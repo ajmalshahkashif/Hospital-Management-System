fbba111fd77fd2b47c7b54edac9ccdd8a42719b7
Ajmal Shah Kashif <ajmalshahkashif@gmail.com>
2020-11-19 21:09:50 +0500

    Name changing from pharmacy to hospital

--- Pharmacy/App_Start/RouteConfig.cs
+++ Pharmacy/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Pharmacy
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Sale", action = "AddSale", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
