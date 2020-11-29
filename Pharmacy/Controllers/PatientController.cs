a64b5f761f08adab1617093fcfc13de54b1e413e
Ajmal Shah Kashif <ajmalshahkashif@gmail.com>
2020-11-29 21:03:45 +0500

    Patient Registartion + some css/JS are removed

---
+++ Pharmacy/Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    public class PatientController : Controller
+    {
+
+        public ActionResult Registration()
+        {
+
+            return View();
+        }
+
+        public ActionResult AllPatients()
+        {
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ Pharmacy/Models/Registration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pharmacy.Models
+{
+    public class Registration
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "provide BarCode")]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string MobileNo { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DOB { get; set; }
+
+
+
+    }
+}
